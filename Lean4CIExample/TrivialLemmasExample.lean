70b6292eab0f4a4236e6a4d138fe5671580bd814
Adomas Baliuka <A.Baliuka@physik.uni-muenchen.de>
2024-02-01 14:44:50 +0100

    Initial commit

---
+++ Lean4CIExample/TrivialLemmasExample.lean
@@ -0,0 +1,12 @@
+/-
+Copyright (c) 2024 Adomas Baliuka. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Adomas Baliuka
+-/
+import Mathlib
+
+lemma something_trivial : 2 + 2 = 4 := rfl
+
+def add_one (n : Nat) : Nat := n + 1
+
+#check Real.log_abs
---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package lean4_CI_example {
+  -- add package configuration options here
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib «Lean4CIExample» {
+  -- add any library configuration options here
+}
